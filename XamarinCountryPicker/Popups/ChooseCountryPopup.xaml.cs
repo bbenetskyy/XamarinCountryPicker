853d8cd71031790bf2d1d905ebe6e2c6caa93d33
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2021-04-04 15:24:55 +0200

    Add Country Popup

---
+++ XamarinCountryPicker/Popups/ChooseCountryPopup.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using FFImageLoading;
+using PhoneNumbers;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using XamarinCountryPicker.Controls;
+using XamarinCountryPicker.Models;
+using XamarinCountryPicker.Utils;
+
+namespace XamarinCountryPicker.Popups
+{
+    public partial class ChooseCountryPopup : PopupPage
+    {
+        #region Fields
+
+        private static List<CountryModel> _countries;
+        private CountryModel _selectedCountry;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public ChooseCountryPopup(CountryModel selectedCountry)
+        {
+            InitializeComponent();
+            if (_countries == null || !_countries.Any())
+            {
+                LoadCountries();
+            }
+            VisibleCountries = new ObservableCollection<CountryModel>(_countries);
+
+            SelectedCountry = selectedCountry;
+            CommonCountriesList.SetBinding(ListView.ItemsSourceProperty, new Binding(nameof(VisibleCountries), source: this));
+            CurrentCountryControl.SetBinding(CountryControl.CountryProperty, new Binding(nameof(SelectedCountry), source: this));
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public ICommand SelectCountryCommand { get; set; }
+
+        public ObservableCollection<CountryModel> VisibleCountries { get; }
+
+        public CountryModel SelectedCountry
+        {
+            get => _selectedCountry;
+            set
+            {
+                _selectedCountry = value;
+                OnPropertyChanged(nameof(SelectedCountry));
+            }
+        }
+
+        #endregion Properties
+
+        #region Private Methods
+
+        private void CloseBtn_Clicked(object sender, EventArgs e)
+        {
+            Rg.Plugins.Popup.Services.PopupNavigation.Instance.PopAsync();
+        }
+
+        private void ConfirmBtn_Clicked(object sender, EventArgs e)
+        {
+            SelectCountryCommand?.Execute(SelectedCountry);
+            Rg.Plugins.Popup.Services.PopupNavigation.Instance.PopAsync();
+        }
+
+        private void LoadCountries()
+        {
+            //this is not Task, because it's really fast
+            var phoneNumberUtil = PhoneNumberUtil.GetInstance();
+            _countries = new List<CountryModel>();
+            var isoCountries = CountryUtils.GetCountriesByIso3166();
+            _countries.AddRange(isoCountries.Select(c => new CountryModel
+            {
+                CountryCode = phoneNumberUtil.GetCountryCodeForRegion(c.TwoLetterISORegionName).ToString(),
+                CountryName = c.EnglishName,
+                FlagUrl = $"https://hatscripts.github.io/circle-flags/flags/{c.TwoLetterISORegionName.ToLower()}.svg",
+                RegionName = c.TwoLetterISORegionName
+            }));
+        }
+
+        private void SearchBar_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            VisibleCountries.Clear();
+            var filteredCountries = string.IsNullOrWhiteSpace(SearchBar.Text)
+                ? _countries
+                : _countries.Where(country => country.CountryName.Contains(SearchBar.Text, StringComparison.InvariantCultureIgnoreCase));
+            filteredCountries.ForEach(сountry => VisibleCountries.Add(сountry));
+        }
+
+        private void CommonCountriesList_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            SelectedCountry = e.SelectedItem as CountryModel;
+        }
+
+        #endregion Private Methods
+    }
+}
---
+++ XamarinCountryPicker/Utils/CountryUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace XamarinCountryPicker.Utils
+{
+    public static class CountryUtils
+    {
+        /// <summary>
+        /// Gets the list of countries based on ISO 3166-1
+        /// </summary>
+        /// <returns>Returns the list of countries based on ISO 3166-1</returns>
+        public static List<RegionInfo> GetCountriesByIso3166()
+        {
+            var countries = new List<RegionInfo>();
+            foreach (var culture in CultureInfo.GetCultures(CultureTypes.SpecificCultures))
+            {
+                var info = new RegionInfo(culture.LCID);
+                if (countries.All(p => p.Name != info.Name))
+                    countries.Add(info);
+            }
+            return countries.OrderBy(p => p.EnglishName).ToList();
+        }
+    }
+}
