75cdcf0776453e2e73f1f4958dd43bf711694cda
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2021-04-04 13:22:41 +0200

    Add Country Control

---
+++ XamarinCountryPicker/Controls/CountryControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+using XamarinCountryPicker.Models;
+
+namespace XamarinCountryPicker.Controls
+{
+    public partial class CountryControl : StackLayout
+    {
+        public static readonly BindableProperty CountryProperty = BindableProperty.Create(
+            nameof(Country),
+            typeof(CountryModel),
+            typeof(CountryControl),
+            default,
+            BindingMode.TwoWay,
+            propertyChanged: (bindable, value, newValue) => (bindable as CountryControl)?.UpdateCountry(newValue as CountryModel));
+
+        public CountryModel Country
+        {
+            get => (CountryModel)GetValue(CountryProperty);
+            set => SetValue(CountryProperty, value);
+        }
+
+        public CountryControl()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateCountry(CountryModel model)
+        {
+            CountryCodeLabel.Text = $"(+{model?.CountryCode})";
+            CountryNameLabel.Text = model?.CountryName;
+            if (!string.IsNullOrEmpty(model?.FlagUrl))
+            {
+                FlagImage.Source = ImageSource.FromUri(new Uri(model?.FlagUrl));
+            }
+        }
+    }
+}
---
+++ XamarinCountryPicker/Models/CountryModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace XamarinCountryPicker.Models
+{
+    public class CountryModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private string _flagUrl;
+        private string _countryName;
+        private string _countryCode;
+        private string _regionName;
+
+        #endregion Fields
+
+        #region Properties
+
+        public string FlagUrl
+        {
+            get => _flagUrl;
+            set => SetProperty(ref _flagUrl, value);
+        }
+
+        public string CountryName
+        {
+            get => _countryName;
+            set => SetProperty(ref _countryName, value);
+        }
+
+        public string CountryCode
+        {
+            get => _countryCode;
+            set => SetProperty(ref _countryCode, value);
+        }
+
+        public string RegionName
+        {
+            get => _regionName;
+            set => SetProperty(ref _regionName, value);
+        }
+
+        #endregion Properties
+
+        protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = "", Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
